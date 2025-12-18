9c84dd6e102c4245c607f66616fb4f9ac49d5b80
Jan Toth <jan.toth138@gmail.com>
2025-12-18 11:16:40 +0100

    Restructure seq experiments

--- experiments/scripts/commands.sh
+++ experiments/scripts/commands.sh
@@ -17,11 +17,22 @@ uv sync
 
 
 # sequences
-uv run auto_counter.py -w ../models/seq/head_tail -inc -o seq_ht_results.csv
-uv run auto_counter.py -w ../models/seq/head_tail/ -d4 -ganak -o seq_ht_results.csv
+for fn in ../models/seq/head_tail/wfomcs
+do
+    timeout 3h uv run auto_counter.py -i $fn -inc -o seq_ht_results_new.csv
+done
 
-uv run auto_counter.py -w ../models/seq/topological_orders -inc -o seq_to_results.csv
-uv run auto_counter.py -w ../models/seq/topological_orders/ -d4 -ganak -o seq_to_results.csv
+for fn in ../models/seq/head_tail/cnf
+do
+    timeout 3h uv run auto_counter.py -i $fn -ganak -o seq_ht_results_new.csv
+    timeout 3h uv run auto_counter.py -i $fn -d4 -o seq_ht_results_new.csv
+done
+
+# uv run auto_counter.py -w ../models/seq/head_tail -inc -o seq_ht_results.csv
+# uv run auto_counter.py -w ../models/seq/head_tail/ -d4 -ganak -o seq_ht_results.csv
+
+# uv run auto_counter.py -w ../models/seq/topological_orders -inc -o seq_to_results.csv
+# uv run auto_counter.py -w ../models/seq/topological_orders/ -d4 -ganak -o seq_to_results.csv
 
 
 # T OOOOOO D OOOOOOOOO
@@ -42,22 +53,22 @@ uv run auto_counter.py -w ../models/seq/topological_orders/ -d4 -ganak -o seq_to
 
 
 # MLNs
-uv run auto_counter.py -w ../models/weather -inc -l inc2 -o mln_results.csv
+# uv run auto_counter.py -w ../models/weather -inc -l inc2 -o mln_results.csv
 
-uv run auto_counter.py -w ../models/weather2/new_encode -inc -l inc2 -o mln_results.csv
-uv run auto_counter.py -w ../models/weather2/old_encode -rec -o mln_results.csv
-uv run auto_counter.py -i ../models/weather2/old_encode/kl4.mln -inc -o mln_results.csv
-uv run auto_counter.py -i ../models/weather2/old_encode/kl5.mln -inc -o mln_results.csv
-uv run auto_counter.py -i ../models/weather2/old_encode/kl6.mln -inc -o mln_results.csv
-uv run auto_counter.py -i ../models/weather2/old_encode/kl7.mln -inc -o mln_results.csv
-uv run auto_counter.py -i ../models/weather2/old_encode/kl8.mln -inc -o mln_results.csv
-uv run auto_counter.py -i ../models/weather2/old_encode/kl9.mln -inc -o mln_results.csv
+# uv run auto_counter.py -w ../models/weather2/new_encode -inc -l inc2 -o mln_results.csv
+# uv run auto_counter.py -w ../models/weather2/old_encode -rec -o mln_results.csv
+# uv run auto_counter.py -i ../models/weather2/old_encode/kl4.mln -inc -o mln_results.csv
+# uv run auto_counter.py -i ../models/weather2/old_encode/kl5.mln -inc -o mln_results.csv
+# uv run auto_counter.py -i ../models/weather2/old_encode/kl6.mln -inc -o mln_results.csv
+# uv run auto_counter.py -i ../models/weather2/old_encode/kl7.mln -inc -o mln_results.csv
+# uv run auto_counter.py -i ../models/weather2/old_encode/kl8.mln -inc -o mln_results.csv
+# uv run auto_counter.py -i ../models/weather2/old_encode/kl9.mln -inc -o mln_results.csv
 
 
-# Watts-Strogatz
-uv run auto_counter.py -w ../models/ws/new_encode -inc -l inc2 -o ws_results.csv
-uv run auto_counter.py -w ../models/ws/old_encode -rec -inc -o ws_results.csv
-uv run auto_counter.py -w ../models/ws/new_encode -ganak -d4 -o ws_results.csv
+# # Watts-Strogatz
+# uv run auto_counter.py -w ../models/ws/new_encode -inc -l inc2 -o ws_results.csv
+# uv run auto_counter.py -w ../models/ws/old_encode -rec -inc -o ws_results.csv
+# uv run auto_counter.py -w ../models/ws/new_encode -ganak -d4 -o ws_results.csv
 
 
 
--- experiments/scripts/generators/sequences.py
+++ experiments/scripts/generators/sequences.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from pathlib import Path
 
@@ -13,15 +14,111 @@ from wfomc.context import WFOMCContext
 
 
 HEAD_TAIL = r"\forall X: (\forall Y: (T(X) & LEQ(X, Y) -> T(Y)))"
-TOPOLOGICAL_ORDERS = r"""\forall X: (\forall Y: (E(X, Y) -> LEQ(X, Y)))"""
+HEAD_MIDDLE_TAIL = r"""
+\forall X: (\forall Y: (T(X) & LEQ(X, Y) -> T(Y))) &
+\forall X: (\forall Y: (H(Y) & LEQ(X, Y) -> H(X))) &
+\forall X: (~T(X) | ~T(Y))
+""".strip()
 
 
-SCRIPT_PATH = Path(__file__).absolute()
-OUTPUT_DIR = SCRIPT_PATH.parent.parent.joinpath("results")  # experiments/results
+EXPERIMENTS_DIR = Path(__file__).absolute().parent.parent.parent  # experiments
+RESULTS_DIR = EXPERIMENTS_DIR.joinpath("results") # experiments/results
+SEQ_MODELS_DIR = EXPERIMENTS_DIR.joinpath("models").joinpath("seq") # experiments/models/seq
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Generate input files for "seqeunce" experiments.',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument('--dom_size', "-n", required=True, type=int)
+    parser.add_argument("--cnf", "-cnf", action="store_true", help="Generate .CNF files")
+    parser.add_argument("--wfomcs", "-wfomcs", action="store_true", help="Generate .WFOMCS files")
+    parser.add_argument("--count", "-count", action="store_true", help="Compute WFOMC for 1,2,3,...,n")
+
+
+    parser.add_argument("--head_tail", "-ht", action="store_true", help="Generate files for HEAD-TAIL sentence")
+    parser.add_argument("--head_middle_tail", "-hmt", action="store_true", help="Generate files for HEAD-MIDDLE-TAIL sentence")
+    parser.add_argument("--combinatorics", "-books", action="store_true", help="Generate files for ENG-MATH-BOOKS (combinatorics id=7) sentence")
+    
+    args = parser.parse_args()
+    return args
+
+
+def write_wfomcs(path, sentence, n):
+    with open(path.joinpath(f"{n}.wfomcs"), "w") as fw:
+        fw.write(sentence)
+        fw.write("\n\n")
+        fw.write(f"domain = {n}\n")
+
+
+def generate_head_tail(n):
+    nvars = n * (n + 1)
+    clauses = []
+
+    leq_id = lambda i, j: n*i + j
+
+    for i in range(1, n + 1):
+        for j in range(1, i):
+            clauses.append([-leq_id(i, j)])
+        for j in range(i, n + 1):
+            leq_i_j = n*i + j
+            clauses.append([leq_id(i, j)])
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            clauses.append([-i, j, -leq_id(i, j)])
+
+    with open(SEQ_MODELS_DIR.joinpath("head_tail").joinpath("cnf").joinpath(f'{n}_e1.cnf'), 'w') as fw:
+        fw.write(f"p cnf {nvars} {len(clauses)}\n")
+        for cl in clauses:
+            for x in cl:
+                fw.write(f"{x} ")
+            fw.write("0\n")
+
+
+def generate_head_middle_tail(n):
+    def next_id():
+        for i in range(1, n**2 + 2*n + 100):
+            yield i
+
+    ids = next_id()
+
+    heads = [next(ids) for i in range(n)]
+    tails = [next(ids) for i in range(n)]
+    leq = [[next(ids) for j in range(n)] for i in range(n)]
+
+    clauses = []
+    for i in range(n):
+        for j in range(i):
+            clauses.append([-leq[i][j]])
+        for j in range(i, n):
+            clauses.append([leq[i][j]])
+
+    for i in range(n):
+        clauses.append([-heads[i], -tails[i]])
+
+        for j in range(n):
+            clauses.append([-heads[i], heads[j], -leq[j][i]])
+            clauses.append([-tails[i], tails[j], -leq[i][j]])
+
+    nvars = n * (n + 2)
+
+    with open(SEQ_MODELS_DIR.joinpath("head_middle_tail").joinpath("cnf").joinpath(f'{n}_e1.cnf'), 'w') as fw:
+        fw.write(f"p cnf {nvars} {len(clauses)}\n")
+        for cl in clauses:
+            for x in cl:
+                fw.write(f"{x} ")
+            fw.write("0\n")
+
+
+def generate_combinatorics_books(n):
+    raise Exception("Use Cofola to generate these files")
 
 
 def run(input, domain_sizes, out_file="seq.csv"):
-    OUTPUT_PATH = os.path.join(OUTPUT_DIR, out_file)
+    OUTPUT_PATH = os.path.join(RESULTS_DIR, out_file)
     if not os.path.exists(OUTPUT_PATH):
         with open(OUTPUT_PATH, "w") as fw:
             fw.write("domain,time,wfomc\n")
@@ -44,11 +141,38 @@ def run(input, domain_sizes, out_file="seq.csv"):
             fw.write(f"{n},{t.elapsed},{res}\n")
 
 
+
+
 if __name__ == "__main__":
     logzero.loglevel(logzero.WARN)
+    args = parse_args()
 
-    if not os.path.exists(OUTPUT_DIR):
-        os.mkdir(OUTPUT_DIR)
+    if args.count:
+        if args.head_tail:
+            run(HEAD_TAIL, 100, "seq_head_tail.csv")
+        
+        if args.head_middle_tail:
+            run(HEAD_MIDDLE_TAIL, 100, "seq_hmt_orders.csv")
 
-    run(HEAD_TAIL, 100, "seq_head_tail.csv")
-    run(TOPOLOGICAL_ORDERS, 100, "seq_top_orders.csv")
+        if args.combinatorics:
+            raise Exception("Use Cofola to generate these files")
+
+    if args.cnf:
+        if args.head_tail:
+            generate_head_tail(args.dom_size)
+
+        if args.head_middle_tail:
+            generate_head_middle_tail(args.dom_size)
+        
+        if args.combinatorics:
+            generate_combinatorics_books(args.dom_size)
+
+    if args.wfomcs:
+        if args.head_tail:
+            write_wfomcs(SEQ_MODELS_DIR.joinpath("head_tail").joinpath("wfomcs"), HEAD_TAIL, args.dom_size)
+
+        if args.head_middle_tail:
+            write_wfomcs(SEQ_MODELS_DIR.joinpath("head_middle_tail").joinpath("wfomcs"), HEAD_MIDDLE_TAIL, args.dom_size)
+
+        if args.combinatorics:
+            raise Exception("Use Cofola to generate these files")
